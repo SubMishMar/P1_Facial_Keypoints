5d1394634a9465ba4419e22f7ddb3c7c32ecac08
subodh <subodh.mishra@ideaforgetech.com>
2025-02-19 18:21:51 +0530

    commit model

--- models.py
+++ models.py
@@ -1,37 +1,54 @@
-## TODO: define the convolutional neural network architecture
-
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
-# can use the below import should you choose to initialize the weights of your Net
-import torch.nn.init as I
-
 
 class Net(nn.Module):
-
     def __init__(self):
         super(Net, self).__init__()
         
-        ## TODO: Define all the layers of this CNN, the only requirements are:
-        ## 1. This network takes in a square (same width and height), grayscale image as input
-        ## 2. It ends with a linear layer that represents the keypoints
-        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
-        
-        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=2)  
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)  
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)  
 
+        # Pooling layer
+        self.pool = nn.MaxPool2d(2, 2)  
+
+        # Compute the correct input size for fc1
+        self._to_linear = None  # Placeholder
         
+        # BatchNorm & Dropout
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.dropout = nn.Dropout(p=0.4)
+        
+        # Fully connected layers (will be set later)
+        self.fc1 = None
+        self.fc2 = None
+
+    def _compute_fc_input_size(self):
+        """Helper function to determine the correct input size for fc1"""
+        with torch.no_grad():
+            dummy_input = torch.zeros(1, 1, 96, 96)  # Example image
+            x = self.pool(F.relu(self.bn1(self.conv1(dummy_input))))
+            x = self.pool(F.relu(self.bn2(self.conv2(x))))
+            x = self.pool(F.relu(self.bn3(self.conv3(x))))
+            self._to_linear = x.view(1, -1).shape[1]  # Compute flattened size
+
+        # Define fully connected layers now
+        self.fc1 = nn.Linear(self._to_linear, 512)
+        self.fc2 = nn.Linear(512, 136)  # Output 136 (68 keypoints * 2)
+
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
-        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+
+        x = x.view(x.size(0), -1)  # Flatten
+
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)  
         
-        
-        # a modified x, having gone through all the layers of your model, should be returned
         return x
